92dbe5fafc0b777dfba3653da51c312334189f3f
Huseyin SAHIN <sakarya.hsa54@gmail.com>
2023-03-14 11:59:55 +0300

    codeCleanUp

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const container = document.querySelector(".container")
-const split = document.querySelectorAll(".split")
+const split = document.querySelectorAll(".split");
 
 split.forEach(element => {
     element.addEventListener("mouseenter", (e) => {
-        e.target.classList.add("active")
+        e.target.classList.add("active");
     });
     element.addEventListener("mouseleave", (e) => {
-        e.target.classList.remove("active")
+        e.target.classList.remove("active");
     });
-
-})
+});
